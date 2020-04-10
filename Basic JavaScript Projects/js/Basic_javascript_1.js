4c8cd84ca984a0cc8ffa0361e6a9c702d1a601a7
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-04-10 15:35:15 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/js/Basic_javascript_1.js
@@ -0,0 +1,15 @@
+var family = "the arezzinis", dad ="jeremiah", mom ="hermoine",
+daughter ="penny", son ="zorro"
+document.write(dad)
+
+var blues = "i have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues)
+
+document.write(2+2)
+
+function my_first_function() {
+    var str = "this text is green";
+    var result = str.fontcolor("green");
+    document.getElementById("green_text").innerHTML = result;
+}
