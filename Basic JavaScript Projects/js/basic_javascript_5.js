cca66daf64197334c5eb879d92d2053ae239f243
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-04-12 22:54:00 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/js/basic_javascript_5.js
@@ -0,0 +1,37 @@
+document.write(typeof 3);
+
+console.log(2 + 2)
+
+document.write("10" + 5)
+
+document.write(18<2)
+
+console.log(false)
+
+document.write(10==10);
+
+document.write(3==11);
+
+x=82;
+y="82";
+document.write(x===y)
+
+x=10;
+y=10;
+document.write(x===y)
+
+a = "marcos";
+b = "nav"
+document.write(a===b)
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(1>15 && 10>1);
+
+document.write(5>10|| 10>4);
+
+document.write(5>10 || 10>20)
+
+function not_function() {
+    document.getElementById("not").innerHTML = !(20>20);
+}
---
+++ Basic JavaScript Projects/js/example.js
@@ -0,0 +1,9 @@
+function my_function() {
+    document.getElementById("test").innerHTML = isNaN('this is a string')
+}
+
+document.write(2e310);
+
+document.write(-3e310)
+
+document.write(10>2);
