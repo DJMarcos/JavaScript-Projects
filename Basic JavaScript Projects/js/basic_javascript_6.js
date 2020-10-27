7f4ae9962e25b77444311dccb31ca64036959cb4
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-10-26 22:01:42 -0700

    Add files via upload

--- Basic JavaScript Projects/js/basic_javascript_6.js
+++ Basic JavaScript Projects/js/basic_javascript_6.js
@@ -16,7 +16,7 @@ var jack = new vehicle("dodge", "viper", 2020,"red");
 var emily = new vehicle("jeep", "trail hawk", 2019, "white and black");
 var erik = new vehicle("ford", "pinto", 1971, "mustard");
 function myfunction() {
-    document.getElementById("keywords_and_constructors").innerHTML ="erick drives a " + erick.vehicle_color + "-colored" + erick.vehicle_model + " manufactured in " + erick.vehicle_year;
+    document.getElementById("keywords_and_constructors").innerHTML ="erick drives a " + erik.vehicle_color + "colored" + erik.vehicle_model + " manufactured in " + erik.vehicle_year;
 }
 
 function count_function() {
@@ -28,3 +28,13 @@ function count_function() {
         return starting_point;
     }
 }
+
+var x = 10;
+function add_numbers_1() {
+    document.write(20 + x + "<br>");
+}
+function add_numbers_2() {
+    document.write(x + 100)
+}
+add_numbers_1()
+add_numbers_2()
